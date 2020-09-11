c733c8927645d08e5b71ca70df1980e8f8e5ed19
Akshay Sharma <aksharma@macbook-pro.lan>
2020-09-11 18:14:11 -0500

    set up dev environment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// TODO: set loginLink
+const loginLink = 'null';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <a href={loginLink}>
+          <button> Log in with Spotify</button>
+        </a>
       </div>
     );
   }
---
+++ src/spotify/API.js
@@ -0,0 +1,14 @@
+import Spotify from 'spotify-web-api-node';
+
+import { atom } from 'recoil';
+
+// TODO: set environment variables for global state
+export const spotifyAPI = atom({
+    key: 'spotifyAPIState',
+    default: new Spotify({
+        clientId: '',
+        clientSecret: '',
+        redirectUri: ''
+    })
+})
+
